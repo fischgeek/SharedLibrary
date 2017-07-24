a274c55319dffea2dab423ae1d04e99b30a2aef7
Jeremiah Fischer <fischgeek@gmail.com>
2017-07-24 14:57:26 -0500

    initial commit

---
+++ Extensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SharedLibrary
+{
+    public static class Extensions
+    {
+        //public static string RegexReplace(this string input, string pattern, RegexOptions options = RegexOptions.IgnoreCase)
+        //{
+        //    return Regex.Replace(input ?? "", pattern, "", options);
+        //}
+        public static string RegexReplace(this string input, string pattern, string replace, RegexOptions options = RegexOptions.IgnoreCase)
+        {
+            return Regex.Replace(input ?? "", pattern, replace, options);
+        }
+
+        public static int GetRandom(this Random random)
+        {
+            Random r = new Random();
+            return r.Next(10);
+        }
+
+		public static bool jfIsNull(this String str)
+		{
+			if (str == null || String.IsNullOrEmpty(str) || String.IsNullOrWhiteSpace(str)) {
+				return true;
+			}
+			return false;
+		}
+
+		public static string ToDateAndTime(this DateTime dt)
+		{
+			return dt.ToString(@"M/dd/yyyy h:mm tt");
+		}
+	}
+}
---
+++ JFUtil.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary
+{
+    public static class JFUtil
+    {
+        /// <summary>
+        /// Gets a random number between 0 and 10 unless max is specified!
+        /// </summary>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static int GetRandom(int max = 10)
+        {
+            Random r = new Random();
+            return r.Next(max);
+        }
+
+        public static int GetRandomNeg(int max = 10)
+        {
+            Random r = new Random();
+            return r.Next(max) * -1;
+        }
+
+        public static decimal GetRandomDecimal()
+        {
+            Random r = new Random();
+            //var a = r.NextDouble() * (10^numOnLeft); //1.54894115  52.56465456465 151.18618316
+            //var xx = Math.Round(a, numOnRight);
+            string s = string.Format("{0}.{1}", GetRandom(), GetRandom(99));
+            decimal d;
+            if (decimal.TryParse(s, out d)) {
+                return d;
+            }
+            return 0;
+        }
+
+		public class JSONResponse
+		{
+			public bool success = true;
+			public string Status { get; set; }
+			public string Message { get; set; }
+			public object Data { get; set; }
+		}
+
+		public enum JSONResponseStatus
+		{
+			Success,
+			Error,
+			None
+		}
+
+		public static string getJSONResponseText(JSONResponseStatus enumStatus)
+		{
+			return enumStatus.ToString();
+		}
+	}
+}
